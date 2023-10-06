2a62a78e8d41dac42d2503bd6995bd609cbcc79c
AlexSteeves <48775637+AlexSteeves@users.noreply.github.com>
2023-10-05 22:41:17 -0400

    first com

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React from 'react';
+import { faker } from "@faker-js/faker";
+import RestartButton from "./Components/RestartButton";
+import GenerateWords from "./Components/GeneratedWords"
+
+
+const words = faker.random.words(10);
+const App = () =>{
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <CountdownTimer timeLeft={30} />
+    <GeneratedWords words={words}/>
+    <RestartButton 
+      className = {"mx-auto mt-10 text-slate-500"}
+      onRestart={()=> null}
+    />
+    
+    </>
   );
 }
+
+const GeneratedWords = ({words}: {words:string }) =>{
+  return <div className = "text-4xl text-center text-slate-500">{words}</div>
+}
+const CountdownTimer = ({ timeLeft}: { timeLeft: number }) => {
+  return <h2 className = "textPrimary-400 font-medium">Time: {timeLeft}</h2>
+}
+
 
 export default App;
---
+++ src/Components/GeneratedWords.tsx
@@ -0,0 +1,10 @@
+const GeneratedWords = ({words}: {words: string}) =>{
+    return(
+        <div className = "text-slate-500">
+            {words}
+
+        </div>
+    );
+};
+
+export default GeneratedWords;
